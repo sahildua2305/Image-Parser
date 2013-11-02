e5d416b9c4954bd256280c3c892c1483358d6985
Sahil <sahildua2305@gmail.com>
2013-11-03 01:19:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<head>
 		<title>Images Parser</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="style.css" />
 		<script>
 			function imgError(img){
 				img.onerror = "";
